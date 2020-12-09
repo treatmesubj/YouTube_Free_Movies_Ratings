251f02d2c927722d69160b33e0f0f519ea3adc4c
John Hupperts <39680353+treatmesubj@users.noreply.github.com>
2020-12-09 11:16:55 -0600

    pluto

--- ytmovies.py
+++ ytmovies.py
@@ -10,11 +10,9 @@ HTTP fetches records of YouTube's available free movies, then fetches and sorts 
 
 
 def get_YT_storefront_dictionary(url):
-
 	"""
 	HTTP fetches YouTube Movies Storefront ytInitialData JavaScript object from a script in the pre-rendered HTML
 	"""
-
 	response = requests.get(url)
 	soup = BeautifulSoup(response.text, "html.parser")
 	pattern = re.compile(r"ytInitialData\s=\s({.*});")
@@ -25,10 +23,11 @@ def get_YT_storefront_dictionary(url):
 	return sanjay
 
 
-if __name__ == "__main__":
-
+def get_YT_free_movies():
+	"""
+	parses YouTube free movies JSON to return a tuple of movies
+	"""
 	home_sanjay = get_YT_storefront_dictionary("https://www.youtube.com/feed/storefront")
-
 	for guy in home_sanjay['contents']['twoColumnBrowseResultsRenderer']['tabs'][0]['tabRenderer']['content']['sectionListRenderer']['contents']:
 		try:
 			if "Free" in guy['itemSectionRenderer']['contents'][0]['shelfRenderer']['title']['runs'][0]['text']:
@@ -36,12 +35,31 @@ if __name__ == "__main__":
 				break
 		except KeyError:
 			pass
-
 	free_sanjay = get_YT_storefront_dictionary(f"https://www.youtube.com{free_movies_url_ext}")
-
 	movies_data = free_sanjay['contents']['twoColumnBrowseResultsRenderer']['tabs'][0]['tabRenderer']['content']['sectionListRenderer']['contents'][0]['itemSectionRenderer']['contents'][0]['shelfRenderer']['content']['gridRenderer']['items']
+	movies = tuple(movie['gridMovieRenderer']['title']['runs'][0]['text'] for movie in movies_data)
 
-	movies = [movie['gridMovieRenderer']['title']['runs'][0]['text'] for movie in movies_data]
+	return movies
+
+
+def get_Pluto_movies():
+	"""
+	HTTP fetches Pluto movies and parses JSON to return tuple of movies
+	"""
+	movies = tuple()
+	response = requests.get("https://service-vod.clusters.pluto.tv/v3/vod/categories?includeItems=true&includeCategoryFields=imageFeatured%2CiconPng&itemOffset=10000&advertisingId=&appName=web&appVersion=5.11.1-2a66a4913105edf0ba49fadd1a6875693b09e78c&app_name=web&clientDeviceType=0&deviceDNT=false&deviceMake=Firefox&deviceModel=&deviceType=web&deviceVersion=83.0&marketingRegion=US&serverSideAds=false&userId=&attributeV4=foo")
+	sanjay = json.loads(response.text) 
+	for category in sanjay['categories']:
+		for item in category['items']:
+			movies += (item['name'],)
+
+	return movies
+
+
+if __name__ == "__main__":
+	# movies = set(get_YT_free_movies() + get_Pluto_movies())
+	
+	movies = get_YT_free_movies()
 
 	movie_ratings = tuple()
 
@@ -49,7 +67,7 @@ if __name__ == "__main__":
 		response = requests.get(f"https://www.rottentomatoes.com/search?search={movie}")
 		soup = BeautifulSoup(response.text, 'html.parser')
 		sanjay = json.loads(soup.select_one("script#movies-json").string)
-		if sanjay['count'] > 0:
+		if sanjay and sanjay['count'] > 0:
 			movie_rating = ((movie, int(sanjay['items'][0]['tomatometerScore'].get('score') or 0), int(sanjay['items'][0]['audienceScore'].get('score') or 0)),)
 			movie_ratings += movie_rating
 			
@@ -59,4 +77,3 @@ if __name__ == "__main__":
 	print('#'*50)
 	for movie in movie_ratings:
 		print(movie)
-	
